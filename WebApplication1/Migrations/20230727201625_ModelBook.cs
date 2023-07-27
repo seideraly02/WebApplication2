52e7220c70212611260c9a8c7c35fb9ae8b9aeac
Sayat <seideraly02@gmail.com>
2023-07-28 03:03:27 +0600

    пройзвел миграцию и обнавил базу данных для клласа Book. И создал все визувлизаци для класса Book но  не разделял по страницам. Тепер можно книги добавлять а выдавть нелзя но кнопки для получения создано

---
+++ WebApplication1/Migrations/20230727201625_ModelBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class ModelBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    CoverPhoto = table.Column<string>(nullable: true),
+                    YearOfManufacture = table.Column<int>(nullable: false),
+                    DescriptionOfTheBook = table.Column<DateTime>(nullable: false),
+                    DateOfAddition = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
